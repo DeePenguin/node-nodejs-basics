5fb8d1931748f8be710d5081c5fbd8ecce2bf544
DeePenguin <alivemushroom@gmail.com>
2024-01-25 17:33:32 +0400

    feat: add zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = join(__dirname, 'files', 'fileToCompress.txt');
+const pathToArchive = join(__dirname, 'files', 'archive.gz');
+const errorMessage = 'Something went wrong';
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const rs = createReadStream(pathToFile);
+    const ws = createWriteStream(pathToArchive);
+    await pipeline(rs, createGzip(), ws);
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { createGunzip } from 'node:zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = join(__dirname, 'files', 'fileToCompress.txt');
+const pathToArchive = join(__dirname, 'files', 'archive.gz');
+const errorMessage = 'Something went wrong';
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const rs = createReadStream(pathToArchive);
+    const ws = createWriteStream(pathToFile);
+    await pipeline(rs, createGunzip(), ws);
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 };
 
 await decompress();
