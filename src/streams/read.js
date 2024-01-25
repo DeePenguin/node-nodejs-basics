1cedf0744c90a7f544cbce8c95e6a5f6845c6313
DeePenguin <alivemushroom@gmail.com>
2024-01-25 12:46:03 +0400

    feat: add streams module

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { stdout } from 'node:process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+const errorMessage = 'Something went wrong while reading the file';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const stream = createReadStream(pathToFile);
+    await pipeline(stream, stdout);
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 };
 
 await read();
