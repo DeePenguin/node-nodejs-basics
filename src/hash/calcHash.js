7b1af00059a09874b8e191e7d1503bf8c28eb18d
DeePenguin <alivemushroom@gmail.com>
2024-01-25 17:11:06 +0400

    feat: add hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from 'node:crypto';
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+import { stdout } from 'node:process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+const hashTs = new Transform({
+  transform(chunk, _, callback) {
+    const hash = createHash('sha256').update(chunk).digest('hex');
+    this.push(hash.concat('\n'));
+    callback();
+  },
+});
+
 const calculateHash = async () => {
-    // Write your code here 
+  const rs = createReadStream(pathToFile);
+  await pipeline(rs, hashTs, stdout);
 };
 
 await calculateHash();
