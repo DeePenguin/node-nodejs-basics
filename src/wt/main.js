07cd15ddc1b029b51681b89fff4cfc1dcc5a12b8
DeePenguin <alivemushroom@gmail.com>
2024-01-27 22:17:09 +0400

    feat: add working threads module

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { availableParallelism } from 'node:os';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Worker } from 'node:worker_threads';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = join(__dirname, 'worker.js');
+const workersAmount = availableParallelism();
+const startNumber = 10;
+
+const createWorker = (path, data) =>
+  new Promise((resolve) => {
+    const worker = new Worker(path, { workerData: data });
+    worker.on('message', (data) => {
+      resolve({ status: 'resolved', data });
+    });
+    worker.on('error', () => {
+      resolve({ status: 'error', data: null });
+    });
+  });
+
 const performCalculations = async () => {
-    // Write your code here
+  const workersPool = Array.from({ length: workersAmount }, (_, index) =>
+    createWorker(pathToFile, index + startNumber)
+  );
+
+  const workersResults = await Promise.all(workersPool);
+
+  console.log(workersResults);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, workerData } from 'node:worker_threads';
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // Uncomment this to simulate random errors
+  // if (Math.random() > 0.7) {
+  //   throw new Error('Simulated error');
+  // }
+
+  parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
